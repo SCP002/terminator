89c8079c8f95a97c984570cd3c52ef25927fac53
SCP002 <feedusfeedme@gmail.com>
2021-05-04 23:24:20 +0300

    Update example.go

--- example/example.go
+++ example/example.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"os/exec"
+	"time"
 
+	"github.com/SCP002/terminator"
+)
+
+func main() {
+	command := exec.Command("notepad")
+	err := command.Start()
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(2 * time.Second)
+	terminator.Stop(command.Process.Pid)
 }
