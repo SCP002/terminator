6355bd88b90181eb02a834133f001d5798038f12
SCP002 <feedusfeedme@gmail.com>
2021-05-05 15:28:58 +0300

    Return error from Stop(), add IsRunning() stub

--- stop_posix.go
+++ stop_posix.go
@@ -6,7 +6,7 @@ import "syscall"
 
 // stop tries to gracefully terminate process with the specified PID
 // by sending SIGTERM to the process or to the process group if such
-// exit
+// exits
 func stop(pid int) {
 	pgid, err := syscall.Getpgid(pid)
 	if err == nil {
--- terminator.go
+++ terminator.go
@@ -1,6 +1,24 @@
 package terminator
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Stop tries to gracefully terminate process with the specified PID
-func Stop(pid int) {
+func Stop(pid int) error {
 	stop(pid)
+
+	running, err := IsRunning(pid)
+	if err != nil {
+		return err
+	}
+	if running {
+		return errors.New("Failed to stop the process with PID: " + fmt.Sprint(pid))
+	}
+	return nil
+}
+
+func IsRunning(pid int) (bool, error) { // TODO: This.
+	return false, nil
 }
