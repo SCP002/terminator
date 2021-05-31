e12d484ddc060858b2ee056d9552937a35845afe
SCP002 <feedusfeedme@gmail.com>
2021-05-31 22:53:18 +0300

    Add build scripts to examples

---
+++ examples/basic/build.cmd
@@ -0,0 +1,27 @@
+@Echo Off
+ChCp 65001 >Nul
+
+Cls
+
+:: Update dependencies
+go get -u ./...
+
+:: Clear unused dependencies
+go mod tidy
+
+:: Build internal dependencies
+PushD "..\..\scripts"
+Call ".\build_dependencies.cmd"
+PopD
+
+:: Build x32
+Set GOOS=windows
+Set GOARCH=386
+go build -o .\terminator_basic_x32.exe terminator_basic.go
+
+:: Build x64
+Set GOOS=windows
+Set GOARCH=amd64
+go build -o .\terminator_basic_x64.exe terminator_basic.go
+
+Exit /B 0
---
+++ examples/separate_batch/build.cmd
@@ -0,0 +1,27 @@
+@Echo Off
+ChCp 65001 >Nul
+
+Cls
+
+:: Update dependencies
+go get -u ./...
+
+:: Clear unused dependencies
+go mod tidy
+
+:: Build internal dependencies
+PushD "..\..\scripts"
+Call ".\build_dependencies.cmd"
+PopD
+
+:: Build x32
+Set GOOS=windows
+Set GOARCH=386
+go build -o .\terminator_separate_batch_x32.exe terminator_separate_batch.go
+
+:: Build x64
+Set GOOS=windows
+Set GOARCH=amd64
+go build -o .\terminator_separate_batch_x64.exe terminator_separate_batch.go
+
+Exit /B 0
