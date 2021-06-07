a2fff8351746aa519513d1d48846f134ebc6b27c
SCP002 <feedusfeedme@gmail.com>
2021-06-07 16:59:57 +0300

    Expand Options structure with Signal parameter; Add comments.

--- terminator.go
+++ terminator.go
@@ -13,12 +13,40 @@ import (
 
 // Options respresents options to stop a process.
 type Options struct {
-	Pid          int    // Identifier of the process to stop.
-	Console      bool   // Is a console process?
-	IgnoreAbsent bool   // Do not return error if process is not running (nothing to stop)?
-	Tree         bool   // Stop the specified process and any child processes which were started by it?
-	Timeout      int    // Time in milliseconds allotted for the process to stop gracefully before it get killed.
-	Answer       string // If not empty, is a message to send to input of the target console after CTRL_C_EVENT is sent.
+	// Identifier of the process to stop.
+	Pid int
+
+	// Is a console process?
+	Console bool
+
+	// Signal type to send if Console is set to "true". 0 = CTRL_C_EVENT, 1 = CTRL_BREAK_EVENT.
+	//
+	// If target process was started with CREATE_NEW_PROCESS_GROUP creation flag and 
+	// SysProcAttr.NoInheritHandles is set to "false", CTRL_C_EVENT will have no effect.
+	//
+	// If target process shares the same console with this one, CTRL_BREAK_EVENT
+	// will stop this process and SetConsoleCtrlHandler can't prevent it.
+	Signal int
+
+	// Do not return error if process is not running (nothing to stop)?
+	IgnoreAbsent bool
+
+	// Stop the specified process and any child processes which were started by it?
+	Tree bool
+
+	// Time in milliseconds allotted for the process to stop gracefully before it get killed.
+	Timeout int
+
+	// If not empty, is a message to send to input of the target console after a signal is sent.
+	//
+	// If a batch executable runs in the same console as a caller,
+	// a prompt is skipped automatically (no need for an answer).
+	//
+	// If this program itself is launched from a batch file
+	// (e.g. run.cmd), prompt appears After this program ends,
+	// thus answering is beyond the scope of this program
+	// (no sense in answering).
+	Answer string
 }
 
 // Stop tries to gracefully terminate the process.
