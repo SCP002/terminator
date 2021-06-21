c5266042c899b1424f41a56247c70503d56abf24
SCP002 <feedusfeedme@gmail.com>
2021-06-21 15:46:36 +0300

    Implement stopping the process tree and sequential signals for POSIX.

--- stop_posix.go
+++ stop_posix.go
@@ -2,19 +2,36 @@
 
 package terminator
 
-import "syscall"
+import (
+	"syscall"
 
-// stop tries to gracefully terminate the process by sending SIGTERM to it or to a process group if such exits and the
-// Tree option is "true".
+	"github.com/shirou/gopsutil/v3/process"
+)
+
+// stop tries to gracefully terminate the process.
 func stop(opts Options) error {
+	// Close each child.
 	if opts.Tree {
-		pgid, err := syscall.Getpgid(opts.Pid)
-		if err == nil {
-			opts.Pid = -pgid
+		list := []*process.Process{}
+		err := GetTree(opts.Pid, &list, false)
+		if err != nil {
+			return err
+		}
+		for _, child := range list {
+			_ = syscall.Kill(int(child.Pid), syscall.SIGINT)
+			_ = syscall.Kill(int(child.Pid), syscall.SIGTERM)
 		}
 	}
-	err := syscall.Kill(opts.Pid, syscall.SIGTERM)
-	if err != nil {
-		return err
-	}
+
+	// Close the root process.
+	_ = syscall.Kill(opts.Pid, syscall.SIGINT)
+	_ = syscall.Kill(opts.Pid, syscall.SIGTERM)
+
+	return nil
+}
+
+// writeAnswer writes an answer message to the console process if specified.
+// TODO: Implement if needed.
+func writeAnswer(pid int, answer string) error {
+	return nil
 }
