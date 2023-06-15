f9a9bbbdf4d1e56fd33f51a30b83b398c2cd02b3
SCP002 <feedusfeedme@gmail.com>
2023-06-15 22:16:37 +0300

    Fix windows proxy binaries trigger false antivirus alarm

--- internal/proxy/build.cmd
+++ internal/proxy/build.cmd
@@ -6,11 +6,13 @@ Cls
 :: Build x32
 Set GOOS=windows
 Set GOARCH=386
-go build -o .\bin\proxy_x32.exe -ldflags "-s -w -H=windowsgui" proxy.go
+:: Do not add both "-s" and "-w" build flags, otherwise antivirus will trigger false alarms
+go build -o .\bin\proxy_x32.exe -ldflags "-s -H=windowsgui" proxy.go
 
 :: Build x64
 Set GOOS=windows
 Set GOARCH=amd64
-go build -o .\bin\proxy_x64.exe -ldflags "-s -w -H=windowsgui" proxy.go
+:: Do not add both "-s" and "-w" build flags, otherwise antivirus will trigger false alarms
+go build -o .\bin\proxy_x64.exe -ldflags "-s -H=windowsgui" proxy.go
 
 Exit /B 0
