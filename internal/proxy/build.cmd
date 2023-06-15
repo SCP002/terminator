0f4fa19bf47a7b93cd0e6f58c973fa3cd1761d77
SCP002 <feedusfeedme@gmail.com>
2023-06-15 11:26:28 +0300

    Omit the symbol table and debug information with "-s" and omit the DWARF symbol table with "-w" in internal/proxy/build.cmd

--- internal/proxy/build.cmd
+++ internal/proxy/build.cmd
@@ -6,11 +6,11 @@ Cls
 :: Build x32
 Set GOOS=windows
 Set GOARCH=386
-go build -o .\bin\proxy_x32.exe -ldflags -H=windowsgui proxy.go
+go build -o .\bin\proxy_x32.exe -ldflags "-s -w -H=windowsgui" proxy.go
 
 :: Build x64
 Set GOOS=windows
 Set GOARCH=amd64
-go build -o .\bin\proxy_x64.exe -ldflags -H=windowsgui proxy.go
+go build -o .\bin\proxy_x64.exe -ldflags "-s -w -H=windowsgui" proxy.go
 
 Exit /B 0
