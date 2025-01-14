49512b70dad2f3b1e96152e485f43e8491dd7d4e
SCP002 <feedusfeedme@gmail.com>
2025-01-14 10:24:56 +0300

    Fix posix build scripts

--- examples/posix/external_pid_sigint/build.sh
+++ examples/posix/external_pid_sigint/build.sh
@@ -6,26 +6,16 @@ clear
 go mod tidy
 
 # Build linux 386
-GOOS=linux
-GOARCH=386
-go build -o "terminator_external_pid_sigint_linux_386" "terminator_external_pid_sigint.go"
+GOOS=linux GOARCH=386 go build -o "terminator_external_pid_sigint_linux_386" "terminator_external_pid_sigint.go"
 
 # Build linux amd64
-GOOS=linux
-GOARCH=amd64
-go build -o "terminator_external_pid_sigint_linux_amd64" "terminator_external_pid_sigint.go"
+GOOS=linux GOARCH=amd64 go build -o "terminator_external_pid_sigint_linux_amd64" "terminator_external_pid_sigint.go"
 
 # Build linux arm64
-GOOS=linux
-GOARCH=arm64
-go build -o "terminator_external_pid_sigint_linux_arm64" "terminator_external_pid_sigint.go"
+GOOS=linux GOARCH=arm64 go build -o "terminator_external_pid_sigint_linux_arm64" "terminator_external_pid_sigint.go"
 
 # Build darwin amd64
-GOOS=darwin
-GOARCH=amd64
-go build -o "terminator_external_pid_sigint_darwin_amd64" "terminator_external_pid_sigint.go"
+GOOS=darwin GOARCH=amd64 go build -o "terminator_external_pid_sigint_darwin_amd64" "terminator_external_pid_sigint.go"
 
 # Build darwin arm64
-GOOS=darwin
-GOARCH=arm64
-go build -o "terminator_external_pid_sigint_darwin_arm64" "terminator_external_pid_sigint.go"
+GOOS=darwin GOARCH=arm64 go build -o "terminator_external_pid_sigint_darwin_arm64" "terminator_external_pid_sigint.go"
--- examples/posix/same_terminal/build.sh
+++ examples/posix/same_terminal/build.sh
@@ -6,26 +6,16 @@ clear
 go mod tidy
 
 # Build linux 386
-GOOS=linux
-GOARCH=386
-go build -o "terminator_same_terminal_linux_386" "terminator_same_terminal.go"
+GOOS=linux GOARCH=386 go build -o "terminator_same_terminal_linux_386" "terminator_same_terminal.go"
 
 # Build linux amd64
-GOOS=linux
-GOARCH=amd64
-go build -o "terminator_same_terminal_linux_amd64" "terminator_same_terminal.go"
+GOOS=linux GOARCH=amd64 go build -o "terminator_same_terminal_linux_amd64" "terminator_same_terminal.go"
 
 # Build linux arm64
-GOOS=linux
-GOARCH=arm64
-go build -o "terminator_same_terminal_linux_arm64" "terminator_same_terminal.go"
+GOOS=linux GOARCH=arm64 go build -o "terminator_same_terminal_linux_arm64" "terminator_same_terminal.go"
 
 # Build darwin amd64
-GOOS=darwin
-GOARCH=amd64
-go build -o "terminator_same_terminal_darwin_amd64" "terminator_same_terminal.go"
+GOOS=darwin GOARCH=amd64 go build -o "terminator_same_terminal_darwin_amd64" "terminator_same_terminal.go"
 
 # Build darwin arm64
-GOOS=darwin
-GOARCH=arm64
-go build -o "terminator_same_terminal_darwin_arm64" "terminator_same_terminal.go"
+GOOS=darwin GOARCH=arm64 go build -o "terminator_same_terminal_darwin_arm64" "terminator_same_terminal.go"
